66ef31fd584743d475e0fd1207f56477a06d6da5
Abdel Mustafa <114242512+samm9812@users.noreply.github.com>
2023-06-08 14:01:05 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,545 @@
+﻿var students = [
+  {
+    name: "Juan Pablo Restrepo Parra",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/juanRestrepo.jpg",
+    videos: [
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/MpNsCDFO3dc"
+      },
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/JT3gk2s0NDk"
+      }, 
+      {
+        description: "Canario electronico",
+        url: "https://www.youtube.com/watch?v=wHnURFf8jOA&t=2s"
+      }, 
+    {
+      description: "Disparador electronico",
+      url:"https://www.youtube.com/embed/CNiuVZLAiRo"
+    },
+    {
+      description:"Sirena de policia",
+      url:"https://www.youtube.com/embed/HJX3xhXpkn0"
+    }
+    ]
+  },
+  {
+    name: "Juan Pablo Gomez Becerra",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/gomez.PNG",
+    videos: [
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/HAKGRulsZ7M"
+      },
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/qqo9uSIkZXY"
+      },
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/JEH8PtsPQms"
+      }
+    
+    ]
+  },
+  {
+    name: "Alejandro Arbelaez",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/alejandro.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/-3udK1NTvMo"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/ItjK77uZvpg"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/Wcdhh6_5rjk"
+      },
+      {
+        description: "Disparador electronico",
+        url: "https://www.youtube.com/embed/fZc-gL-vr1M"
+      }
+    ]
+  },
+  {
+    name: "Valeria Orozco Rodriguez",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/val.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/D077atedDa0"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/DAzKomKm_lY"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/Wcdhh6_5rjk"
+      },
+      {
+        description: "Disparador electronico",
+        url: "https://www.youtube.com/embed/7inEZTuwkps"
+      }
+    ]
+  },
+  {
+    name: "Dayana Estefania Castillo",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/DayanaCastillo.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/WZFpqKlr3c8"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/XQMspGWhNfA"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/8D3e1yaxLXU"
+      },
+      {
+        description: "Disparador electronico",
+        url: "https://www.youtube.com/embed/U85p64li9fs"
+      },
+      {
+        description: "Sirena de policia",
+        url: "https://www.youtube.com/embed/S6XpBA3Q0Vg"
+      }
+    ]
+  },
+  {
+    name: "Christelle Bermudez Velez",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/bermudez.PNG.jpg",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/1DnFZRagXi0"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/LbB749xNfZ0"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/CpKE0F6enJI"
+      },
+      {
+        description: "Disparador electronico",
+        url:"https://www.youtube.com/embed/wPa5eGKNSkU"
+      },
+      {
+        description: "Sirena de policia",
+        url: "https://www.youtube.com/embed/KANrJNV-ABk"
+      }
+    ]
+  },
+  {
+    name: "Juan Sebastian Garcia serna",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/Garcia.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/7XR6-ZdVR_4"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/x0ucNmz7pfE"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/KabvBRzVlUQ"
+      }
+    ]
+  },
+  {
+    name: "Ricardo Dorado",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/ricardo.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/r_-rxRvQRT4"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/7NzIreb46Uc"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/5w-H4dId9oM"
+      }
+    ]
+  },
+  {
+    name: "Karen Dayana Henao Sanchez",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/henao.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/cqQ1sP-kdyU"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/SJqslbYUUHs"
+      },
+      {
+        description: "canario electronico",
+        url: "https://www.youtube.com/embed/vHpFfy09JcM"
+      }
+    ]
+  },
+  {
+    name: "Jean Paul Naranjo",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/naranjio.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/hRTTHxuUkaU"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/y5CINeuB_wE"
+      },
+      {
+        description: "",
+        url: ""
+      }
+    ]
+  },
+  {
+    name: "Sebastian Pareja Loaiza",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/pareja.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/O244jLOLIQk"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/y5CINeuB_wE"
+      },
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/spxl92uhGVk"
+      },
+      {
+        description: "Disparador Electronico",
+        url: "https://www.youtube.com/embed/7gUnvS7sB8k"
+      },
+      {
+        description: "Sirena de policia",
+        url: "https://www.youtube.com/embed/57kO_wkN45E"
+      }
+    ]
+  },
+  {
+    name: "Kevin Stiven Grisales",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/grisales.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/IBoDD8FZRsE"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/KICvoInSlNI"
+      },
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/yS4YkHuOSTY"
+      },
+      {
+        description:"Disparador electronico",
+        url: "https://www.youtube.com/embed/uMM_chye6UA"
+      },
+      {
+        description: "Sirena de policia",
+        url: "https://www.youtube.com/embed/w8iyq14mVYA"
+      }
+    ]
+  },
+  {
+    name: "Sebastian Murcia Serrano",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/murcia.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/2w6Tllnxduk"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/42esEiZauXY"
+      },
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/pPFBVJHpUrM"
+      },
+      {
+        description: "Sirena de policia",
+        url:"https://www.youtube.com/embed/kWnbUGURqt8"
+      }
+    ]
+  },
+  {
+    name: "Oscar David Obando",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/obando.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/cbih86kHVzs"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/DJuzARC_k8w"
+      },
+      {
+        description: "Canario y Disparador electronico",
+        url: "https://www.youtube.com/embed/gWjfzkJaU0g"
+      },
+      {
+        description: " Sirena de policia",
+        url:"https://www.youtube.com/embed/G1xOpA7T4FA"
+      }
+     
+    ]
+  },
+  {
+    name: "Juan David Botero",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "pics/botero.PNG",
+    videos: [
+      {
+        description: "temporizador variable",
+        url: "https://www.youtube.com/embed/WkLAZYaDlaU"
+      },
+      {
+        description: "luz nocturna",
+        url: "https://www.youtube.com/embed/YWZOlAYut2U"
+      },
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/ZbjbA-xM4cI"
+      },
+      {
+        description: "Disparador electrnoico",
+        url: "https://www.youtube.com/embed/6xDiT1gWSSo"
+      },
+      {
+        description: "Sirena de policia",
+        url: "https://www.youtube.com/embed/uJf8_8OeP-s"
+      }
+    ]
+  },
+  {
+    name: "Juan Sebastian Garcia Loaiza",
+    semester: 1,
+    program: "ingenieria electronica",
+    photo: "\Users\CASA\Desktop\galeria\assets\juan",
+    videos: [
+    
+      {
+        description: "Canario Electronico",
+        url: "https://www.youtube.com/embed/nZGsRimXj_U"
+      },
+      {
+        description: "Disparador electronico",
+        url:"https://www.youtube.com/embed/-Bh5NnwXqC4"
+      },
+      {
+        description:"Sirena de policia",
+        url:"https://www.youtube.com/embed/uwr8rZw3f24"
+      }
+    ]
+  }
+];
+
+function filterGallery() {
+  var filter = document.getElementById("search-input").value.toUpperCase();
+  var studentBoxes = document.getElementsByClassName("student-box");
+  var contactSection = document.getElementById("contact-section");
+
+  for (var i = 0; i < studentBoxes.length; i++) {
+    var studentBox = studentBoxes[i];
+    var studentName = studentBox.getElementsByTagName("h2")[0].textContent.toUpperCase();
+
+    if (studentName.includes(filter)) {
+      studentBox.style.display = "block";
+    } else {
+      studentBox.style.display = "none";
+    }
+  }
+
+  if (filter === "") {
+    for (var i = 0; i < studentBoxes.length; i++) {
+      studentBoxes[i].style.display = "inline-block"; // Restablecer la posición original
+    }
+    contactSection.style.display = "block"; // Mostrar la sección de contacto cuando no hay filtro
+  } else {
+    contactSection.style.display = "none"; // Ocultar la sección de contacto cuando hay filtro
+  }
+}
+
+
+
+
+function createViewVideosHandler(studentIndex) {
+  return function () {
+    var studentVideosSection = document.getElementById("student-videos-" + studentIndex);
+    var studentVideos = students[studentIndex].videos;
+
+    var allVideoSections = document.getElementsByClassName("student-video-section");
+    for (var i = 0; i < allVideoSections.length; i++) {
+      allVideoSections[i].style.display = "none";
+    }
+
+    studentVideosSection.style.display = "block";
+    studentVideosSection.innerHTML = "";
+
+    var videoContainer = document.createElement("ul");
+    videoContainer.className = "video-container"; // Agregar la clase "video-container"
+
+    for (var j = 0; j < studentVideos.length; j++) {
+      var video = studentVideos[j];
+
+      var videoItem = document.createElement("li");
+      videoItem.className = "video-item"; // Agregar la clase "video-item"
+
+      var videoDescription = document.createElement("p");
+      videoDescription.textContent = video.description;
+
+      var videoElement = document.createElement("iframe");
+      videoElement.src = video.url;
+      videoElement.width = "560";
+      videoElement.height = "315";
+      videoElement.allowfullscreen = true; // Agregar el atributo allowfullscreen
+      videoElement.setAttribute("allow", "fullscreen"); // Alternativa para mayor compatibilidad
+
+      videoItem.appendChild(videoDescription);
+      videoItem.appendChild(videoElement);
+
+      videoContainer.appendChild(videoItem);
+    }
+
+    studentVideosSection.appendChild(videoContainer);
+
+    var backButton = document.createElement("button");
+    backButton.textContent = "Volver";
+    backButton.className = "back-button";
+    backButton.addEventListener("click", function () {
+      studentVideosSection.style.display = "none";
+      var studentBoxes = document.getElementsByClassName("student-box");
+      for (var k = 0; k < studentBoxes.length; k++) {
+        if (k !== studentIndex) {
+          studentBoxes[k].classList.remove("hide");
+        }
+      }
+    });
+
+    studentVideosSection.appendChild(backButton);
+
+    var studentBoxes = document.getElementsByClassName("student-box");
+    for (var k = 0; k < studentBoxes.length; k++) {
+      if (k !== studentIndex) {
+        studentBoxes[k].classList.add("hide");
+      }
+    }
+  };
+}
+
+
+
+function createGallery() {
+  var studentsContainer = document.getElementById("students-container");
+
+  for (var i = 0; i < students.length; i++) {
+    var student = students[i];
+
+    var studentBox = document.createElement("div");
+    studentBox.className = "student-box";
+
+    var studentInfo = document.createElement("div");
+    studentInfo.className = "student-info";
+
+    var studentPhoto = document.createElement("img");
+    studentPhoto.src = student.photo;
+    studentPhoto.alt = "no envio foto y no sale en video para screenshot"
+    studentInfo.appendChild(studentPhoto);
+
+    var studentDetails = document.createElement("div");
+    studentDetails.innerHTML = "<h2>" + student.name + "</h2>" +
+      "<p>Semestre: " + student.semester + "</p>" +
+      "<p>Programa: " + student.program + "</p>";
+    studentInfo.appendChild(studentDetails);
+
+    var viewVideosButton = document.createElement("button");
+    viewVideosButton.textContent = "Ver Videos";
+    viewVideosButton.className = "view-videos-button";
+    viewVideosButton.addEventListener("click", createViewVideosHandler(i));
+
+    studentBox.appendChild(studentInfo);
+    studentBox.appendChild(viewVideosButton);
+
+    var studentVideosSection = document.createElement("div");
+    studentVideosSection.id = "student-videos-" + i;
+    studentVideosSection.className = "student-video-section";
+
+    studentBox.appendChild(studentVideosSection);
+
+    studentsContainer.appendChild(studentBox);
+  }
+}
+function toggleMenu() {
+  var menuItems = document.querySelector('.menu-items');
+  menuItems.style.display = menuItems.style.display === 'none' ? 'block' : 'none';
+}
+
+
+window.onload = function () {
+  createGallery();
+
+  var searchInput = document.getElementById("search-input");
+  
+searchInput.addEventListener("keyup", filterGallery);
+
+searchInput.addEventListener("keyup", filterGallery);
+searchInput.addEventListener("blur", filterGallery);
+
+
+};
+
+ 
